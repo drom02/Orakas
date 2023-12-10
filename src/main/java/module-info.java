496a6c9110907c9d4965ee248cf4c66bc2b260db
drom02 <drom02@vse.cz>
2023-12-10 13:04:16 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module graphics.sorter {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens graphics.sorter to javafx.fxml;
+    exports graphics.sorter;
+}
